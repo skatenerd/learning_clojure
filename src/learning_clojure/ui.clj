dc33de3ed825abde8c6ca37c37206509cdd53dc9
Wai Lee <skatenerd@gmail.com>
2011-12-25 18:16:19 -0600

    i made stuff commit in a more sophisticated way

--- src/learning_clojure/ui.clj
+++ src/learning_clojure/ui.clj
@@ -1,6 +1,6 @@
 (ns learning_clojure.ui
   "i am experimenting with ui"
-  (:require [clojure.contrib.seq-utils :as seq-utils]
+  (:require [clojure.contrib.generic.math-functions :as math]
             )
   )
 
@@ -15,8 +15,8 @@
           ]
       m-name)))
 (findmethods java.awt.Frame (re-pattern "[vV]is"))
-(def hgt 500)
-(def wdth 500)
+(def hgt 800)
+(def wdth 800)
 (def frame (java.awt.Frame.))
 (doto frame
   (.setVisible true)
@@ -25,6 +25,7 @@
 (doto gfx
   (.setColor (java.awt.Color. 255 128 0))
   )
+(comment
 (defn draw-ball [posn]
   (if (< posn 300)
   (send-off *agent* #'draw-ball))
@@ -33,7 +34,31 @@
     (.fillOval posn 150 50 50))
   (. Thread (sleep 200))
   (+ 30 posn))
-  
-(def worldstate (ref 0))
+  )
+
+
+(def starting-posns
+  (for [y (range 0 (int (/ hgt 2)) 150)]
+    [0 y]))
+(def ball-agents (map agent starting-posns))
+(def worldstate (vec ball-agents))
+(println worldstate)
 (def drawagent (agent 0))
-(send-off drawagent draw-ball)
+(defn draw-world [_]
+  (send-off *agent* #'draw-world)
+  (doto gfx
+    (.clearRect 0 0 wdth hgt))
+  (doseq [curball worldstate]
+    (let [[x y] @curball]
+      (.fillOval gfx x y 50 50)))
+  (. Thread (sleep 200))
+  nil)
+(defn traverse [[x y]]
+  (if (< x wdth)
+  (send-off *agent* #'traverse))
+  (. Thread (sleep 200))
+  [(+ x 50) (+ y (* 80 (math/sin (* 6.28 (/ x 360)))))])
+
+(send-off drawagent draw-world)
+(doseq [cur-ball ball-agents]
+  (send-off cur-ball traverse))
